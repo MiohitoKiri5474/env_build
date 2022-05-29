862cc775a7395209abd2d062e34f10baada3e296
miohitokiri5474 <lltzpp@gmail.com>
2022-05-29 21:24:14 +0800

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,1 +1,8 @@
 sudo apt install python3 python3-pip neovim neofetch htop zsh zip unzip thefuck tmux
+pip3 install pynvim
+unzip zshrc.zip
+mv .oh_my_zsh/.zshrc .oh_my_zsh/.p10k.zsh
+unzip nvim.zip
+mkdir .config
+mv nvim .config
+nvim +PlugUpdate +qa
