695373e53289d474543814fb7ed11efbcc54e576
miohitokiri5474 <lltzpp@gmail.com>
2022-05-30 13:01:52 +0800

    update build, adding tmux configs

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,21 @@
-sudo apt install python3 python3-pip neovim neofetch htop zsh zip unzip thefuck tmux
+sudo apt install python3 python3-pip neofetch htop zsh zip unzip thefuck tmux
+sudo apt install -y aria2
 pip3 install pynvim
+
 unzip zshrc.zip
-mv .oh_my_zsh/.zshrc .oh_my_zsh/.p10k.zsh
+mv .oh_my_zsh/ .zshrc ~
+
+unzip tmuxrc.zip
+mv .tmux/ .tmux.conf .tmux.conf.local ~
+
+git clone https://github.com/neovim/neovim
+cd neovim
+make
+sudo make install
+cd ~
+
 unzip nvim.zip
-mkdir .config
-mv nvim .config
+mkdir ~/.config
+mv nvim ~/.config/
+nvim +UpdateRemotePlugins +qa
 nvim +PlugUpdate +qa
